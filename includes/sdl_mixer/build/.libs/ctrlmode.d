610f6efb6e3fc507220e11340cc3faf037f2dea8
sofian casier <casier_s@sofian-HP-EliteBook-8560p.(none)>
2014-05-19 15:40:17 +0200

    pl

--- includes/sdl_mixer/build/.libs/ctrlmode.d
+++
@@ -1,8 +0,0 @@
-build/ctrlmode.lo: timidity/ctrlmode.c timidity/config.h \
- /usr/local/include/SDL2/SDL_config.h \
- /usr/local/include/SDL2/SDL_platform.h \
- /usr/local/include/SDL2/begin_code.h \
- /usr/local/include/SDL2/close_code.h \
- /usr/local/include/SDL2/SDL_endian.h \
- /usr/local/include/SDL2/SDL_stdinc.h \
- /usr/local/include/SDL2/SDL_config.h timidity/ctrlmode.h
--- includes/sdl_mixer/build/.libs/sdl_a.d
+++
@@ -1,8 +0,0 @@
-build/sdl_a.lo: timidity/sdl_a.c timidity/config.h \
- /usr/local/include/SDL2/SDL_config.h \
- /usr/local/include/SDL2/SDL_platform.h \
- /usr/local/include/SDL2/begin_code.h \
- /usr/local/include/SDL2/close_code.h \
- /usr/local/include/SDL2/SDL_endian.h \
- /usr/local/include/SDL2/SDL_stdinc.h \
- /usr/local/include/SDL2/SDL_config.h timidity/output.h
